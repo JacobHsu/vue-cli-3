28144c2683f5cdb7310e20f9d84eeb8dbc837483
jacobhsu <jacob.hsu.tw@gmail.com>
2020-02-27 18:06:04 +0800

    fix vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 // vue.config.js
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? 'vue-cli-3' : '/',
+    // github path error /vue-cli-3/vue-cli-3
+    // devServer: {
+    //     open: true,
+    //     host: 'localhost',  // 設置主機地址
+    //     port: 8080,         // 設置默認埠號
+    //     https: false,
+    //     //以上的ip和埠號是我們本機的;下面為需要跨域的
+    //     proxy: {    //配置跨域
+    //      '/api': {
+    //       target: 'http://localhost:8080',
+    //       //ws: true,     // 如果要代理 websockets
+    //       //changOrigin: true,  //允許跨域
+    //       pathRewrite: {
+    //        '^/api': '/mock' //請求的時候使用這個api就可以
+    //       }
+    //      }
+    //     }
+    // }
 }
